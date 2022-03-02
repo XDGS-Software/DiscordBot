387a272f9dc6c2f501edd694c9db26bf3592cc3e
GStudiosX2 <ramazanogutlu0@gmail.com>
2022-03-02 21:05:16 +0000

    Add cooldown

--- src/events/CommandEvent.ts
+++ src/events/CommandEvent.ts
@@ -2,6 +2,15 @@ import { Message } from "discord.js";
 
 let { prefix } = process.env;
 
+let cooldown = {};
+async function cooldownIt(message: Message, name: string, ms: number, reaction: string, callback: Function) {
+	if((cooldown[`${message.author.id}-${name}`]||null)==null){
+		callback();
+		setTimeout(()=>{cooldown[`${message.author.id}-${name}`]=null;},ms);
+		cooldown[`${message.author.id}-${name}`]=true;
+	}else{await message.react(reaction);}
+}
+
 module.exports = {
 	name: 'messageCreate',
 	once: false,
@@ -17,7 +26,19 @@ module.exports = {
         		await message.reply('This is a test command.');
 				break;
 			case "site":
-				await message.reply('Our Site: https://xdgs.gstudiosx.tk/');
+				await cooldownIt(message, 'siteCommandCooldown', 3500, '⌛', async () => {
+					await message.reply('Our Site: https://xdgs.gstudiosx.tk/');
+				});
+				break;
+			case "tos":
+				await cooldownIt(message, 'siteCommandCooldown', 3500, '⌛', async () => {
+					await message.reply('Our T.O.S: https://xdgs.gstudiosx.tk/tos');
+				});
+				break;
+			case "privacy":
+				await cooldownIt(message, 'siteCommandCooldown', 3500, '⌛', async () => {
+					await message.reply('Our Privacy Policy: https://xdgs.gstudiosx.tk/privacy');
+				});
 				break;
 		}
 	},
